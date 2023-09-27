a0324abbfce0474b0a0595c8e3b14ddb14356f31
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-09-27 21:38:08 +0300

    Setup league View Controller Table View of Leagues

---
+++ SportsApp/Constants/K.swift
@@ -0,0 +1,17 @@
+//
+//  K.swift
+//  SportsApp
+//
+//  Created by Mustafa on 27/09/2023.
+//
+
+import Foundation
+
+
+struct K {
+    
+    static let customNibNameLeagues = "CustomNibCell"
+    static let customNibNameIdentfier = "LeagueCell"
+    static let customNibNameLeaguesHeight = 90
+    
+}
---
+++ SportsApp/Modules/Leagues/ViewLeagues/ViewNibLeagues/LeaguesViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LeaguesViewController.swift
+//  SportsApp
+//
+//  Created by Mustafa on 27/09/2023.
+//
+
+import UIKit
+
+class LeaguesViewController: UIViewController {
+
+    @IBOutlet weak var leaguesTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//MARK: - Function Needed using In League TableView TableView
+extension LeaguesViewController{
+    
+    func configureLeagueTableView(){
+        leaguesTableView.dataSource = self
+        leaguesTableView.delegate = self
+        leaguesTableView.register(UINib(nibName: K.customNibNameLeagues, bundle: nil), forCellReuseIdentifier: K.customNibNameIdentfier)
+    }
+    
+}
+
+
+//MARK: - Adding Data Source To League Table View
+extension LeaguesViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.customNibNameIdentfier, for: indexPath) as? CustomNibCell ?? UITableViewCell()
+        
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
+
+//MARK: - Adding Delegate to League Table View
+extension LeaguesViewController: UITableViewDelegate {
+    
+}
