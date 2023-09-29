52f4ef20244f9bc3264bf30272d92572e55689f2
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-09-29 22:05:05 +0300

    Set enviroment Of network and Model and Be Suitable For calling Api

--- SportsApp/Constants/APIConstant.swift
+++ SportsApp/Constants/APIConstant.swift
@@ -8,17 +8,104 @@
 import Foundation
 
 struct APIConstant{
-    private static let ApiKey = "10450c41674e1753658fa155979ccb6e59c7649b49a5a0cd3508b5e823f50499"
-    static let mainSportsURl = "https://allsportsapi.com/"
-    static let soccerSportsURl = "https://apiv2.allsportsapi.com/football/"
-    static let basketballSportsURl = "https://apiv2.allsportsapi.com/basketball/"
-    static let cricketSportsURl = "https://apiv2.allsportsapi.com/cricket/"
-    static let tennisSportsURl = "https://apiv2.allsportsapi.com/tennis/"
-    static var sportType:String?
     
-    static let allLeagues = "mainSportsURl\(sportType ?? "football"+ApiKey)"
-    static let getUpComingEvent = "https://apiv2.allsportsapi.com/football?met=Fixtures&leagueId=[leagueId]&from=[CurrentDate]&to=[CurrentDate + OneYear]&APIkey=[YourKey]"
-    static let getLatestResult = "https://apiv2.allsportsapi.com/football?met=Fixtures&leagueId=[leagueId]&from=[CurrentDate - OneYear]&to=[CurrentDate]&APIkey=[YourKey]"
     
+    private  let ApiKey = "10450c41674e1753658fa155979ccb6e59c7649b49a5a0cd3508b5e823f50499"
+     private let mainSportsURl = "https://apiv2.allsportsapi.com/"
+     private let sportType:String
+     private let met:String
+     private var leagueId:Int?
+     private var teamId:Int?
+    
+    var URlAApi: String {
+        get{
+            
+            return "\(mainSportsURl)\(sportType)\(met)\(ApiKey)"
+        }
+    }
+    init(sportType:String ){
+        
+        self.sportType = sportType
+        self.met = Met.allLeagues.order
+    }
+    init(sportType:String , leagueIdWithUpcomiingEvent:Int){
+        
+        self.sportType = sportType
+        self.met = Met.getUpcomingEvents(leagueIdWithUpcomiingEvent).order
+        self.leagueId = leagueIdWithUpcomiingEvent
+    }
+    init(sportType:String , leagueIdWithLatestEvent:Int){
+        
+        self.sportType = sportType
+        self.met = Met.getLatestResults(leagueIdWithLatestEvent).order
+        self.leagueId = leagueIdWithLatestEvent
+    }
+    
+    init(sportType:String , teamId:Int){
+        
+        self.sportType = sportType
+        self.met = Met.getTeamDetails(teamId).order
+        self.teamId = teamId
+    }
+   
+   
+    enum Met {
+        case allLeagues
+        case getUpcomingEvents(Int)
+        case getLatestResults(Int)
+        case getTeamDetails(Int)
+        
+        var order:String{
+            switch self {
+            case .allLeagues: return "/?met=Leagues&APIkey="
+                
+            case .getUpcomingEvents (let leagueID): return "?met=Fixtures&leagueId=\(leagueID)&from=\(Date.getCurrentDate())&to=\(Date.getNextYeatDate())&APIkey="
+            case .getLatestResults (let leagueID): return "?met=Fixtures&leagueId=\(leagueID)&from=\(Date.getLastYeatDate())&to=\(Date.getCurrentDate())&APIkey="
+            case .getTeamDetails (let teamID): return "?&met=Teams&teamId=\(teamID)&APIkey="
+            }
+        }
+    }
+    
+    
+    
+
     
 }
+// MARK: ADding extenision to The Date To Get Current ANd Next And Last Year
+extension Date {
+    static func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        return dateFormatter.string(from: Date())
+
+        }
+    
+    static func getNextYeatDate() -> String {
+            
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return dateFormatter.string(from:calendar.date(byAdding: .year, value: 1, to: Date()) ?? Date())
+
+        }
+    
+    static func getLastYeatDate() -> String {
+            
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        var dateComponents = DateComponents()
+        dateComponents.year = -1
+
+        return dateFormatter.string(from:calendar.date(byAdding: dateComponents , to: Date()) ?? Date())
+
+        }
+    
+
+}
+
+
+
