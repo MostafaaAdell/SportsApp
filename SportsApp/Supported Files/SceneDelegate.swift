dae7432e5dbd3181814925e023099758c5fc1a7e
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-09-29 03:20:34 +0300

    Updates From Abdulla and adding Main StoryBoard

--- SportsApp/Supported Files/SceneDelegate.swift
+++ SportsApp/Supported Files/SceneDelegate.swift
@@ -16,12 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let scene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(windowScene: scene)
-        let rootViewController = LeaguesViewController()
-        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
-        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: scene)
+//        let rootViewController = LeaguesVC()
+//        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -57,4 +56,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
